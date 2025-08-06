822734b306a84b08366e30c222f6eea7776bcc27
fResult <styxmaz@gmail.com>
2025-08-06 21:11:41 +0700

    1st commit.

---
+++ src/test/kotlin/com/fresult/testing/TestingApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.fresult.testing
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TestingApplicationTests {
+
+  @Test
+  fun contextLoads() {
+  }
+
+}
