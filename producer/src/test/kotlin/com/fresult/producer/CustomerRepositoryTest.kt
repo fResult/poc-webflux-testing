55160bd6c4f7c6a49cf88065287c2ec743e76bfc
fResult <styxmaz@gmail.com>
2025-08-08 23:48:00 +0700

    Create `find be name` unit test case.

--- producer/src/test/kotlin/com/fresult/producer/CustomerRepositoryTest.kt
+++ producer/src/test/kotlin/com/fresult/producer/CustomerRepositoryTest.kt
@@ -1,15 +1,22 @@
 package com.fresult.producer
 
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest
 import org.springframework.test.context.DynamicPropertyRegistry
 import org.springframework.test.context.DynamicPropertySource
 import org.testcontainers.containers.MongoDBContainer
 import org.testcontainers.junit.jupiter.Container
 import org.testcontainers.junit.jupiter.Testcontainers
+import reactor.core.publisher.Flux
+import reactor.test.StepVerifier
 
 @Testcontainers
 @DataMongoTest
 class CustomerRepositoryTest {
+  @Autowired
+  lateinit var customerRepository: CustomerRepository
+
   companion object {
     @Container
     val mongoDbContainer: MongoDBContainer = MongoDBContainer("mongo:8.0.12")
@@ -17,5 +24,28 @@ class CustomerRepositoryTest {
     @DynamicPropertySource
     fun setProperties(registry: DynamicPropertyRegistry): Unit =
       registry.add("spring.data.mongodb.uri", mongoDbContainer::getReplicaSetUrl)
+  }
+
+
+  @Test
+  fun `find by name`() {
+    val commonName = "Jane"
+    val customer1 = Customer("1", commonName)
+    val customer2 = Customer("2", "John")
+    val customer3 = Customer("3", commonName)
+
+    val setup = customerRepository.deleteAll()
+      .thenMany(
+        customerRepository.saveAll(Flux.just(customer1, customer2, customer3))
+      ).thenMany(
+        customerRepository.findByName(commonName)
+      )
+
+    val isCommonName: (Customer) -> Boolean = { customer -> customer.name.equals(commonName, true) }
+
+    StepVerifier.create(setup)
+      .expectNextMatches(isCommonName)
+      .expectNextMatches(isCommonName)
+      .verifyComplete()
   }
 }
