274a28245c32a8e2bfca4013f21bb7e6e6af27b9
fResult <styxmaz@gmail.com>
2025-08-09 01:48:54 +0700

    Create `CustomerRepository` interface.

---
+++ producer/src/test/kotlin/com/fresult/producer/CustomerRepository.kt
@@ -0,0 +1,9 @@
+package com.fresult.producer
+
+import org.springframework.data.mongodb.repository.ReactiveMongoRepository
+import reactor.core.publisher.Flux
+
+
+interface CustomerRepository : ReactiveMongoRepository<Customer, String> {
+  fun findByName(string: String): Flux<Customer>
+}
