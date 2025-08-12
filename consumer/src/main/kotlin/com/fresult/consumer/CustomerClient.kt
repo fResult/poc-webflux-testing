697f1c45e11c1a694da638facd5c62d337624579
fResult <styxmaz@gmail.com>
2025-08-12 15:13:07 +0700

    Prepare `setupWireMock` for each test.

--- consumer/src/main/kotlin/com/fresult/consumer/CustomerClient.kt
+++ consumer/src/main/kotlin/com/fresult/consumer/CustomerClient.kt
@@ -1,6 +1,17 @@
 package com.fresult.consumer
 
+import org.apache.logging.log4j.LogManager
+import org.apache.logging.log4j.Logger
 import org.springframework.stereotype.Component
 
 @Component
-class CustomerClient
+class CustomerClient {
+  companion object {
+    private val log: Logger = LogManager.getLogger(CustomerClient::class.java)
+  }
+  var base: String = ""
+    set(value) {
+      field = value
+      log.info("Base URL set to: $field")
+    }
+}
--- consumer/src/test/kotlin/com/fresult/consumer/WiremockCustomerClientTest.kt
+++ consumer/src/test/kotlin/com/fresult/consumer/WiremockCustomerClientTest.kt
@@ -1,9 +1,13 @@
 package com.fresult.consumer
 
+import com.github.tomakehurst.wiremock.client.WireMock
+import org.junit.jupiter.api.BeforeEach
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.cloud.contract.wiremock.AutoConfigureWireMock
 import org.springframework.core.env.Environment
+import org.springframework.http.HttpHeaders
+import org.springframework.http.MediaType
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @AutoConfigureWireMock(port = 0)
@@ -13,4 +17,27 @@ class WiremockCustomerClientTest {
 
   @Autowired
   private lateinit var environment: Environment
+
+  @BeforeEach
+  fun setupWireMock() {
+    val wiremockServerPort = environment.getProperty("wiremock.server.port", Int::class.java) ?: 0
+    val base = "localhost:$wiremockServerPort"
+    customerClient.base = base
+
+    val json = """
+      [
+        {"id": "1", "name": "John Wick"},
+        {"id": "2", "name": "Thomas Anderson"}
+      ]
+    """.trimIndent()
+
+    WireMock.stubFor(
+      WireMock.get("/customers")
+        .willReturn(
+          WireMock.aResponse()
+            .withHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+            .withBody(json)
+        )
+    )
+  }
 }
