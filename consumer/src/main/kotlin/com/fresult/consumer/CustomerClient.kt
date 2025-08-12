a58b15e1d23f28826f4ae85f2154c285bf2da136
fResult <styxmaz@gmail.com>
2025-08-12 15:59:33 +0700

    Implement `CustomerClient#getCustomers` method.

--- consumer/src/main/kotlin/com/fresult/consumer/CustomerClient.kt
+++ consumer/src/main/kotlin/com/fresult/consumer/CustomerClient.kt
@@ -3,11 +3,13 @@ package com.fresult.consumer
 import com.fresult.producer.Customer
 import org.apache.logging.log4j.LogManager
 import org.apache.logging.log4j.Logger
+import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.stereotype.Component
+import org.springframework.web.reactive.function.client.WebClient
 import reactor.core.publisher.Flux
 
 @Component
-class CustomerClient {
+class CustomerClient(@Qualifier("webCustomerClient") private val client: WebClient) {
   companion object {
     private val log: Logger = LogManager.getLogger(CustomerClient::class.java)
   }
@@ -19,6 +21,9 @@ class CustomerClient {
     }
 
   fun getCustomers(): Flux<Customer> {
-    TODO("Not yet implemented")
+    return client.get()
+      .uri("http://$base/customers")
+      .retrieve()
+      .bodyToFlux(Customer::class.java)
   }
 }
