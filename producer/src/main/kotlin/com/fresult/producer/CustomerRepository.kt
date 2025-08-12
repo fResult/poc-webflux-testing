7ae6ea70eb97b6e7c362b3cb2e96e585af7e2c5f
fResult <styxmaz@gmail.com>
2025-08-12 11:23:20 +0700

    Move `CustomerRepository` interface to the proper directory.

--- producer/src/main/kotlin/com/fresult/producer/CustomerRepository.kt
+++ producer/src/main/kotlin/com/fresult/producer/CustomerRepository.kt
@@ -3,7 +3,6 @@ package com.fresult.producer
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository
 import reactor.core.publisher.Flux
 
-
 interface CustomerRepository : ReactiveMongoRepository<Customer, String> {
   fun findByName(string: String): Flux<Customer>
 }
