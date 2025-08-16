e7444d64bcd901d7237b402730a489605877d672
fResult <styxmaz@gmail.com>
2025-08-16 16:51:56 +0700

    Create `StubRunnerCustomerClientTest# get all customers` to test API across service.

---
+++ consumer/src/test/kotlin/com/fresult/consumer/StubRunnerCustomerClientTest.kt
@@ -0,0 +1,42 @@
+package com.fresult.consumer
+
+import com.fresult.producer.CustomerResponse
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.cloud.contract.stubrunner.spring.AutoConfigureStubRunner
+import org.springframework.cloud.contract.stubrunner.spring.StubRunnerPort
+import org.springframework.cloud.contract.stubrunner.spring.StubRunnerProperties
+import reactor.test.StepVerifier
+import kotlin.test.Test
+
+@SpringBootTest(
+  webEnvironment = SpringBootTest.WebEnvironment.MOCK,
+  classes = [ConsumerApplication::class],
+)
+@AutoConfigureStubRunner(
+  ids = [StubRunnerCustomerClientTest.PRODUCER_ARTIFACT_ID],
+  stubsMode = StubRunnerProperties.StubsMode.LOCAL,
+)
+class StubRunnerCustomerClientTest {
+  companion object {
+    const val PRODUCER_ARTIFACT_ID = "com.fresult:producer"
+  }
+
+  @Autowired
+  private lateinit var customerClient: CustomerClient
+
+  @StubRunnerPort(PRODUCER_ARTIFACT_ID)
+  private val producerServicePort = 0
+
+  @Test
+  fun `get all customers`() {
+    val base = "localhost:$producerServicePort"
+    customerClient.base = base
+    val customers = customerClient.getCustomers()
+
+    StepVerifier.create(customers)
+      .expectNext(CustomerResponse("1", "John Wick"))
+      .expectNext(CustomerResponse("2", "Thomas Anderson"))
+      .verifyComplete()
+  }
+}
